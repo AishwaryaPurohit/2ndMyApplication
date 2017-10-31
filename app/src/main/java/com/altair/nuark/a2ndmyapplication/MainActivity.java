16658334d23ac1e29275462f524f72d1a8785ec7
nuarkHcs <info@nuarkhcs.com>
2017-10-31 13:47:24 +0530

    Initial commit

---
+++ app/src/main/java/com/altair/nuark/a2ndmyapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package com.altair.nuark.a2ndmyapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
